aa63ef5632aeff018325f8c540ccc2e8d57a8681
DARIELADOLFO <darieladolfopenavasquez@gmail.com>
2022-08-02 00:49:47 -0400

    mi pagina web

---
+++ script.js
@@ -0,0 +1,36 @@
+//FunciÃ³n que me aplica el estilo a la opciÃ²n seleccionada y quita la previamente seleccionada
+function seleccionar(link) {
+    var opciones = document.querySelectorAll('#links  a');
+    opciones[0].className = "";
+    opciones[1].className = "";
+    opciones[2].className = "";
+    opciones[3].className = "";
+    opciones[4].className = "";
+      link.className = "seleccionado";
+}
+
+//funciÃ³n que muestra el menu responsive
+function responsiveMenu() {
+    var x = document.getElementById("nav");
+    if (x.className === "") {
+        x.className = "responsive";
+    } else {
+        x.className = "";
+    }
+}
+
+//detecto el scrolling para aplicar la animaciÃ³n del la barra de habilidades
+window.onscroll = function() { efectoHabilidades() };
+
+//funcion que aplica la animaciÃ³n de la barra de habilidades
+function efectoHabilidades() {
+    var skills = document.getElementById("skills");
+    var distancia_skills = window.innerHeight - skills.getBoundingClientRect().top;
+    if (distancia_skills >= 300) {
+        document.getElementById("html").classList.add("barra-progreso1");
+        document.getElementById("js").classList.add("barra-progreso2");
+        document.getElementById("so").classList.add("barra-progreso3");
+        document.getElementById("ps").classList.add("barra-progreso4");
+       }
+
+}
